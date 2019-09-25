cac34ad934638d616d1b497b97b9e09194b744ba
LongBody <nguyenthanhlong1120@gmail.com>
2019-09-25 17:49:10 +0700

    new

--- codejs.js
+++ codejs.js
@@ -2,15 +2,21 @@ var d = new Date();
 document.getElementById("date").innerHTML = d
 
 
-$(document).ready(function() {
-    $('.navbar a.dropdown-toggle').on('click', function(e) {
-        var $el = $(this);
-        var $parent = $(this).offsetParent(".dropdown-menu");
-        $(this).parent("li").toggleClass('open');
-        if (!$parent.parent().hasClass('nav')) {
-            $el.next().css({ "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 });
-        }
-        $('.nav li.open').not($(this).parents("li")).removeClass("open");
-        return false;
-    });
-});
+function show_search_advance() {
+    // Lấy thẻ a và category
+    var a = document.getElementById('show_search_advance');
+    var category = document.getElementById('search_advance');
+    // Lấy nội dung của thẻ a
+    var text = a.innerHTML;
+    // Kiểm tra xử lý hiển thị và ẩn + đổi nội dung thẻ a
+    if (text == 'Tìm kiếm nâng cao') {
+        category.style.display = "table-row";
+        a.innerHTML = "Bỏ tìm kiếm nâng cao";
+    } else {
+        category.style.display = "none";
+        a.innerHTML = "Tìm kiếm nâng cao";
+    }
+
+    // Return false để khi click vào thẻ a sẽ không bị chuyển trang
+    return false;
+}
