38e6c08c0577327bb957a63a3df5f6eb73405e2e
LongBody <nguyenthanhlong1120@gmail.com>
2019-10-02 23:11:18 +0700

    new

--- codejs.js
+++ codejs.js
@@ -1,22 +1,17 @@
 var d = new Date();
 document.getElementById("date").innerHTML = d
 
+var x = document.getElementById('xem_them')
+var y = document.getElementById('more');
+var z = document.getElementById('more0')
+var b = document.getElementById('cla')
+y.addEventListener('click', function() {
+    x.style.display = 'inline'
+    z.appendChild(x)
 
-function show_search_advance() {
-    // Lấy thẻ a và category
-    var a = document.getElementById('show_search_advance');
-    var category = document.getElementById('search_advance');
-    // Lấy nội dung của thẻ a
-    var text = a.innerHTML;
-    // Kiểm tra xử lý hiển thị và ẩn + đổi nội dung thẻ a
-    if (text == 'Tìm kiếm nâng cao') {
-        category.style.display = "table-row";
-        a.innerHTML = "Bỏ tìm kiếm nâng cao";
-    } else {
-        category.style.display = "none";
-        a.innerHTML = "Tìm kiếm nâng cao";
-    }
+})
+y.addEventListener('dblclick', function() {
+    b.style.display = 'inline'
+    x.appendChild(b)
 
-    // Return false để khi click vào thẻ a sẽ không bị chuyển trang
-    return false;
-}
+})
